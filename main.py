85da9a2a2d3798768cd283bd31a41ba3c958f2a6
hammyover90001 <101239896+hammyover90001@users.noreply.github.com>
2023-05-29 23:38:13 -0700

    Update main.py
    added some comments to make it more readable

--- main.py
+++ main.py
@@ -14,7 +14,8 @@ def resource_path(relative_path):
         base_path = os.path.abspath(".")
 
     return os.path.join(base_path, relative_path)
-
+# so all the stuff above i have no idea how it workds becauese i followed a yt tutorial. 
+#this one>>>> https://www.youtube.com/watch?v=p3tSLatmGvU <<<<if you are interested
 def mains():
     loading_root.destroy()
     cmd = 'python mainwin.py'
@@ -22,6 +23,8 @@ def mains():
     out,err = p.communicate()
     print(err)
     print(out)
+    #this basicall just closes down the window and starts to run mainwin.py which is the other file, which is the core game. 
+    #the reason I did this is because otherwise opencv would not work. I dunno why, it just didnt wanna run
 
 def instructions():
     def destroy():
@@ -47,6 +50,7 @@ def instructions():
 
     instructions_back = Button(instructions,text="Return", font = ("Times",20),command = destroy)
     instructions_back.pack(pady=10)
+    #this is the instructions tab that opens up. its pretty basic
 def usure():
     def quitt():
         loading_root.quit()
@@ -61,6 +65,7 @@ def usure():
     x = (screenwidth / 2) - (Windowh / 2)
     y = (screenhieght / 2) - (Windowl / 2)
     u.geometry(f'{Windowh}x{Windowl}+{int(x)}+{int(y)}')
+    #this centres the window the the middle of any screen on any computer. 
     u.title('u sure?')
     ulabel = Label(u,bg = "#52C7D5" , text = "Are you sure you want to quit?",font = ("Times",30))
     ulabel.pack()
@@ -71,7 +76,8 @@ def usure():
     spacer.pack(pady=80)
     ubutton = Button(u,text = "Yes",bg = "#F4827B",font = ("Times",5),command = quitt)
     ubutton.pack(pady= 10)
-
+    #this is the "you sure" button that comes up whenever you try to close anythign ever. 
+    
 
 loading_root = Tk()
 
@@ -82,9 +88,11 @@ screenhieght = loading_root.winfo_screenheight()
 x = (screenwidth / 2) - (Windowh / 2)
 y = (screenhieght / 2) - (Windowl / 2)
 loading_root.geometry(f'{Windowh}x{Windowl}+{int(x)}+{int(y)}')
+# agian this centres it
 
 loading_root.configure(bg = "#52C7D5")
 loading_root.title("launcher.exe")
+#everything below this is just buttons. they are pretty self explanitory
 loading_label = Label(loading_root, text="GEO PARTY",bg = "#52C7D5", font=("Times", 30))
 loading_label.pack(pady=20)
 
