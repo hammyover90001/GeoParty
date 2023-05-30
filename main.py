b747e3647cc90e9e0262d118d1f825162e52edd0
hammyover90001 <101239896+hammyover90001@users.noreply.github.com>
2023-05-29 23:28:10 -0700

    Add files via upload

---
+++ main.py
@@ -0,0 +1,101 @@
+import subprocess
+global usrimage
+from tkinter import *
+from tkinter import Tk, Label
+import sys
+import os
+#https://stackoverflow.com/questions/31836104/pyinstaller-and-onefile-how-to-include-an-image-in-the-exe-file
+def resource_path(relative_path):
+  #  """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS2
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+
+def mains():
+    loading_root.destroy()
+    cmd = 'python mainwin.py'
+    p = subprocess.Popen(cmd, shell=True)
+    out,err = p.communicate()
+    print(err)
+    print(out)
+
+def instructions():
+    def destroy():
+        instructions.destroy()
+    instructions = Tk()
+
+    Windowh = 1000
+    Windowl = 200
+    screenwidth = instructions.winfo_screenwidth()
+    screenhieght = instructions.winfo_screenheight()
+    x = (screenwidth / 2) - (Windowh / 2)
+    y = (screenhieght / 2) - (Windowl / 2)
+    instructions.geometry(f'{Windowh}x{Windowl}+{int(x)}+{int(y)}')
+
+    instructions_l = Label(instructions,text="To play, you will first have to select a level, buy clicking forwards or back (difficulty is shown)",font=("Times",18))
+    instructions_l.pack()
+    instructions_l2 = Label(instructions,text="Then you view the image and try to find that location in real life.",font=("Times",18))
+    instructions_l2.pack()
+    instructions_l3 = Label(instructions, text="Once you find the location, you will have to line up the angle just right,",font=("Times",18))
+    instructions_l3.pack()
+    instructions_l4 = Label(instructions, text="Then you upload it to the program, and then it will tell you if you succeeded or not",font=("Times",18))
+    instructions_l4.pack()
+
+    instructions_back = Button(instructions,text="Return", font = ("Times",20),command = destroy)
+    instructions_back.pack(pady=10)
+def usure():
+    def quitt():
+        loading_root.quit()
+        u.quit()
+    def uitt():
+        u.destroy()
+    u = Tk()
+    Windowh = 500
+    Windowl = 360
+    screenwidth = loading_root.winfo_screenwidth()
+    screenhieght = loading_root.winfo_screenheight()
+    x = (screenwidth / 2) - (Windowh / 2)
+    y = (screenhieght / 2) - (Windowl / 2)
+    u.geometry(f'{Windowh}x{Windowl}+{int(x)}+{int(y)}')
+    u.title('u sure?')
+    ulabel = Label(u,bg = "#52C7D5" , text = "Are you sure you want to quit?",font = ("Times",30))
+    ulabel.pack()
+    u.configure(bg = "#52C7D5")
+    uno = Button(u,text = "I'm no wuss",bg = "#81C784" ,font =  ("Times",30),command = uitt)
+    uno.pack(pady= 10)
+    spacer = Label(u,bg = "#52C7D5")
+    spacer.pack(pady=80)
+    ubutton = Button(u,text = "Yes",bg = "#F4827B",font = ("Times",5),command = quitt)
+    ubutton.pack(pady= 10)
+
+
+loading_root = Tk()
+
+Windowh = 500
+Windowl = 360
+screenwidth = loading_root.winfo_screenwidth()
+screenhieght = loading_root.winfo_screenheight()
+x = (screenwidth / 2) - (Windowh / 2)
+y = (screenhieght / 2) - (Windowl / 2)
+loading_root.geometry(f'{Windowh}x{Windowl}+{int(x)}+{int(y)}')
+
+loading_root.configure(bg = "#52C7D5")
+loading_root.title("launcher.exe")
+loading_label = Label(loading_root, text="GEO PARTY",bg = "#52C7D5", font=("Times", 30))
+loading_label.pack(pady=20)
+
+loading_button = Button(loading_root, text="P  L  A  Y",bg = "#48C2D2", font=("Times", 30), command=mains)
+loading_button.pack(pady=20)
+
+loading_play = Button(loading_root, text="How To Play", bg = "#48C2D2", font=("Times", 18), command=instructions)
+loading_play.pack(pady=20)
+
+quit_button = Button(loading_root,bg = "#F4827B",text="I accdentally launched the program and dont want to play it so can I please leave",font=("Times", 11), command = usure)
+quit_button.pack(pady=20)
+
+
+mainloop()
---
+++ mainwin.py
@@ -0,0 +1,212 @@
+from tkinter import *
+from PIL import ImageTk, Image
+import cv2
+import numpy as np
+from tkinter import Tk, Label
+from tkinter import filedialog
+
+import sys
+import os
+#https://stackoverflow.com/questions/31836104/pyinstaller-and-onefile-how-to-include-an-image-in-the-exe-file
+def resource_path(relative_path):
+  #  """ Get absolute path to resource, works for dev and for PyInstaller """
+    try:
+        # PyInstaller creates a temp folder and stores path in _MEIPASS
+        base_path = sys._MEIPASS2
+    except Exception:
+        base_path = os.path.abspath(".")
+
+    return os.path.join(base_path, relative_path)
+#^^^ that is the extension that will compare the two images and retun a % value of their similarity.
+root = Tk()
+difficulty = 0.7
+#so these are the images
+root.configure(bg = "#52C7D5")
+
+# VERY IMPORTANT!
+# VERY IMPORTANT!
+
+# VERY IMPORTANT!
+# VERY IMPORTANT!
+# VERY IMPORTANT!
+#if you want this code to run, you will have to change all of the directories to where you saved the images.
+# if you scroll down to the next set of images, (not this one, the one below)
+#you will have to change all of the directories, to where the folder ended up.
+#if you need help, just let me know and i will help you figure it out.
+#i attached the folder in the submition
+# basically it will look something like this: "drive/locaion/location/imagechecks/c1"
+
+root.title("geo party")
+image1 = ImageTk.PhotoImage(Image.open(resource_path("levels/l1.png")))
+image2 = ImageTk.PhotoImage(Image.open(resource_path("levels/l2_2.jpg")))
+image3 = ImageTk.PhotoImage(Image.open(resource_path("levels/l3.jpg")))
+image4 = ImageTk.PhotoImage(Image.open(resource_path("levels/l4.jpg")))
+image5 = ImageTk.PhotoImage(Image.open(resource_path("levels/l5.jpg")))
+image6 = ImageTk.PhotoImage(Image.open(resource_path("levels/l6.jpg")))
+image7 = ImageTk.PhotoImage(Image.open(resource_path("levels/l7.jpg")))
+image8 = ImageTk.PhotoImage(Image.open(resource_path("levels/l8.jpg")))
+image9 = ImageTk.PhotoImage(Image.open(resource_path("levels/l9.jpg")))
+image10 = ImageTk.PhotoImage(Image.open(resource_path("levels/L10.jpg")))
+image11 = ImageTk.PhotoImage(Image.open(resource_path("levels/l11_2.jpg")))
+image12 = ImageTk.PhotoImage(Image.open(resource_path("levels/l12_2.jpg")))
+image13 = ImageTk.PhotoImage(Image.open(resource_path("levels/l13_2.jpg")))
+image14 = ImageTk.PhotoImage(Image.open(resource_path("levels/l14_2.jpg")))
+image15 = ImageTk.PhotoImage(Image.open(resource_path("levels/l15_2.jpg")))
+thanks = ImageTk.PhotoImage(Image.open(resource_path("levels/thanks.png")))
+#made an image list to make it easier to use
+image_list = [image1, image2, image3, image4, image5, image6, image7, image8, image9, image10,image11,image12,image13,image14,image15,thanks]
+# basically this just tells you which image/level you are on
+status = Label(root, text= "Level 1 of " + str(len(image_list)), bg = "#52C7D5", bd=1, font=('Times', 20))
+usrin = ""
+level = 0
+def find_template(level):
+    global labl_results
+    template = ""
+    #texts = "calculating"
+    #labl_results.config(text = texts)
+    #need it to update but it wont work?
+#you will have to change the direcotry for this stuff too... sorry :/
+    print(level)
+    print(level)
+    print(level)
+    level = level-1
+    if level ==1:
+        template = resource_path("checks/c1.png")
+    if level == 2:
+        template = resource_path("checks/c1.png")
+    if level == 3:
+        template = resource_path("checks/c3.png")
+    if level ==4:
+        template = resource_path("checks/c4.png")
+    if level == 5:
+        template = resource_path("checks/c5.png")
+    if level == 6:
+        template = resource_path("checks/c6.png")
+    if level ==7:
+        template = resource_path("checks/c7.png")
+    if level == 8:
+        template = resource_path("checks/c8.png")
+    if level == 9:
+        template = resource_path("checks/c9.png")
+    if level ==10:
+        template = resource_path("checks/c10.png")
+    if level == 11:
+        template = resource_path("checks/c11.png")
+    if level == 12:
+        template = resource_path("checks/c12.png")
+    if level == 13:
+        template = resource_path("checks/c13.png")
+    if level == 14:
+        template = resource_path("checks/c14.png")
+    if level == 15:
+        template = resource_path("checks/c15.png")
+    filepath = filedialog.askopenfilename()
+    # Load the image
+    img = cv2.imread(filepath)
+    temp = cv2.imread(template)
+    res = cv2.matchTemplate(img, temp, cv2.TM_CCOEFF_NORMED)
+    print("got here")
+    threshold = 0.6
+
+    #^^ that changes the difficulty. we could make it so the player can choose the difficulty and say that .4 is easy, .6 is normal, and .8+ is hard, and 1 is impossible
+    loc = np.where(res >= threshold)
+    print(res)
+    if len(loc[0]) == 0:
+        print("fail")
+        labl_results = Label(root, text="LEVEL FAILED YOU LOOSER",bg = "#52C7D5", font=('Times',25)).grid(row=9,column=0)
+        return False
+
+    labl_results = Label(root, text="CONGRANTS. YOU DONE IT!",bg = "#52C7D5", font=('Times',25)).grid(row=9,column=0)
+    print("pass")
+    return True
+# button loads the next image
+def forward(level_num):
+    global Labell
+    global button_forward
+    global button_back
+    Labell.grid_forget()
+    global level
+    global button_print
+    global DIFFICULTY
+    global labl_results
+    labl_results = Label(root, text = "           Submit to get results           " ,bg = "#52C7D5", font=('Times', 25)).grid(row=9, column=0)
+    #^rests the box so the next image can load
+    Labell = Label(image = image_list[level_num-1])
+    #^this gets the image
+    button_forward = Button(root, text="Next",font=("times",18), bg = "#64B5F6", command = lambda: forward(level_num+1))
+    button_back=Button(root, text="Previous",font=("times",18), bg = "#64B5F6", command= lambda : back(level_num-1))
+
+    #^ forward and backwards
+    if level_num == 16:
+        button_forward = Button(root, text="Next",font=("times",18), bg = "#64B5F6", state= DISABLED)
+        button_print = Button(root, text="Submit Image", font=('Times', 25), bg="#4CAF50",state=DISABLED).grid(row=10, column=1, columnspan=40, pady=20)
+    #^stops it from going past 10
+    if level_num<5:
+        DIFFICULTY = Label(root, bg="#66BB6A", text="DIFFICULTY = GREEN (baby)", font=("Times", 30)).grid(row=9, column=1)
+    if level_num >= 5:
+        DIFFICULTY = Label(root, bg="#FFD54F", text="DIFFICULTY = ORANGE ( :/ )", font=("Times", 30)).grid(row=9, column=1)
+    if level_num>= 10:
+        DIFFICULTY = Label(root, bg="#FF7043", text="    DIFFICULTY = RED ( >:) )  ", font=("Times", 30)).grid(row=9, column=1)
+    Labell.grid(row=1, column=0, columnspan=3)
+    button_back.grid(row = 2, column=0,padx=280)
+    button_forward.grid(row = 2, column=1)
+    #^the position
+
+    status = Label(root, text="Level "+ str(level_num) +" of " + str(len(image_list)), bd=1, font=('Times', 20), bg = "#52C7D5")
+    status.grid(row=0, column=0, columnspan=3, sticky=W + E)
+    level = level_num
+
+
+#this is the exact same as the forward button
+def back(level_num):
+    global Labell
+    global button_forward
+    global button_back
+    global level
+    Labell.grid_forget()
+    global DIFFICULTY
+    global labl_results
+    global button_print
+    button_print = Button(root, text="Submit Image", font=('Times', 25), bg="#4CAF50",command=lambda: find_template(level + 1)).grid(row=10, column=1, columnspan=40, pady=20)
+    labl_results = Label(root, text="           Submit to get results           ", bg="#52C7D5",font=('Times', 25)).grid(row=9, column=0)
+    if level_num < 5:
+
+        DIFFICULTY = Label(root, bg="#66BB6A", text="DIFFICULTY = GREEN (baby)", font=("Times", 30)).grid(row=9, column=1)
+    if level_num >= 5:
+        DIFFICULTY = Label(root, bg="#FFD54F", text="DIFFICULTY = ORANGE ( :/ )", font=("Times", 30)).grid(row=9, column=1)
+    if level_num >= 10:
+        DIFFICULTY = Label(root, bg="#FF7043", text="    DIFFICULTY = RED ( >:) )  ", font=("Times", 30)).grid(row=9, column=1)
+    Labell.grid_forget()
+    Labell = Label(image=image_list[level_num - 1])
+    button_forward = Button(root, text="Next", bg = "#64B5F6", font=("times",18), command=lambda: forward(level_num + 1))
+    button_back = Button(root, text="Previous", bg = "#64B5F6",font=("times",18), command=lambda: back(level_num - 1))
+    if level_num == 1:
+        button_back = Button(root, text="Previous", bg = "#64B5F6",font=("times",18), state=DISABLED)
+
+    Labell.grid(row=1, column=0, columnspan=3)
+    button_back.grid(row=2, column=0,padx=280)
+    button_forward.grid(row=2, column=1)
+
+    status = Label(root, text="Level "+ str(level_num) +" of " + str(len(image_list)), bd=1, font=('Times', 20), bg = "#52C7D5")
+    status.grid(row=0, column=0, columnspan=3, sticky=W + E)
+    level = level_num
+
+Labell = Label(image = image1)
+Labell.grid(row=1, column=0,columnspan=3)
+
+
+#the first image should be the tutorial thing
+#it will basically explain how the game works and how to submit
+#or like first 3 images
+#then the game actually starts
+labl_results = Label(root,bg = "#52C7D5", text="           Submit to get results           ", font=('times',25)).grid(row=9,column=0)
+#usrimage = np.array(Image.open(usrin))
+#down below are all the buttons and whatnot
+
+button_print = Button(root,text="Submit Image",font=('Times',25),bg = "#4CAF50", command = lambda: find_template(level+1)).grid(row = 10 ,column=1,columnspan=40, pady=20)
+DIFFICULTY = Label(root, bg = "#66BB6A", text="DIFFICULTY = GREEN (baby)",font=("Times", 30)).grid(row = 9, column=1)
+button_back = Button(root,text="Previous", bg = "#64B5F6", font=('Times', 18), command = back).grid(row = 2, column=0,padx=280)
+button_forward = Button(root,text="Next", bg = "#64B5F6", font=('Times', 18), command = lambda : forward(2)).grid(row = 2, column=1)
+
+status.grid(row=0, column=0, columnspan=3, sticky= W+E)
+root.mainloop()
